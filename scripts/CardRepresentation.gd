ab77b83c1e98e40dda8f50867f4f70d3a41b68e9
Dvlv <dvd292@googlemail.com>
2018-10-21 08:37:12 +0100

    cards go inactive after attack

--- scripts/CardRepresentation.gd
+++ scripts/CardRepresentation.gd
@@ -14,6 +14,7 @@ var is_on_field = false
 var is_in_hand = false
 var is_opponent_card = false
 var my_slot = null
+var is_inactive = false
 
 onready var my_card_script_node = Node2D.new()
 
@@ -32,12 +33,13 @@ func _ready():
 func _on_TextureButton_pressed():
 	emit_signal("active_card", CARD_RESOURCE)
 	
-	if is_on_field and not is_opponent_card:
-		$FieldMenu.visible = true
-	elif is_on_field and is_opponent_card and globals.is_in_attack_choose_state:
-		$OpponentFieldMenu.visible = true
-	elif is_in_hand:
-		$HandMenu.visible = true
+	if not is_inactive:
+		if is_on_field and not is_opponent_card:
+			$FieldMenu.visible = true
+		elif is_on_field and is_opponent_card and globals.is_in_attack_choose_state:
+			$OpponentFieldMenu.visible = true
+		elif is_in_hand:
+			$HandMenu.visible = true
 
 
 func set_card_power_and_health():
@@ -60,7 +62,17 @@ func connect_attack_and_effect_signals():
 		my_card_script_node.connect("damage_opponent_card", self, "emit_damage_opponent_card")
 		my_card_script_node.connect("damage_opponent", self, "emit_damage_opponent")
 		my_card_script_node.connect("declare_attack", self, "emit_declare_attack")
-	
+
+
+func set_inactive():
+	is_inactive = true
+	$TextureButton.modulate = "333333"
+
+
+func set_active():
+	is_inactive = false
+	$TextureButton.modulate = "ffffff"
+
 
 func close_menus():
 	$FieldMenu.visible = false
--- scripts/MatchLogic.gd
+++ scripts/MatchLogic.gd
@@ -1,44 +1,50 @@
 extends Node2D
 
 
-func _ready():
-	pass
-	
 func set_deck(deck_card_resources):
 	for res in deck_card_resources:
 		$Deck.add_card(res)
 		$Deck.shuffle()
-		
+
+
 func fill_hand():
 	while $Hand.get_next_empty_slot():
 		$Hand.add_card($Deck.draw(), false)
 	connect_all_hand_card_reps()
 
+
 func connect_all_hand_card_reps():
 	$Hand.connect_active_signal_to_all_children(self)
 	$Hand.connect_play_signal_to_all_children(self)
-	
+
+
 func connect_all_board_card_reps():
 	$Board.connect_active_signal_to_all_children(self)
 	$Board.connect_attack_signal_to_all_children(self)
-	
+
+
 func update_active_card(card_res):
 	$Hand.close_all_menus()
 	$Board.close_all_menus()
 	$ActiveCard.set_new_resource(card_res)
-	
+
+
 func play_card(slot):
 	var card_in_slot = slot.get_card()
 	slot.remove_card()
 	$Board.add_player_card(card_in_slot.CARD_RESOURCE)
 	connect_all_board_card_reps()
-	
-	
+
+
 func do_card_battle(player_card, opp_card):
 	$Hand.close_all_menus()
 	$Board.close_all_menus()
 	opp_card.take_damage(player_card.CARD_RESOURCE.POWER)
 	player_card.take_damage(opp_card.CARD_RESOURCE.POWER)
+
+
+func post_attack(attacking_card_rep):
+	attacking_card_rep.set_inactive()
 
 
 func damage_opponent(dmg):
@@ -52,11 +58,13 @@ func on_damage_opponent_card(dmg):
 func on_damage_opponent(dmg):
 	print("damaging opponent for ", dmg)
 	damage_opponent(dmg)
-	
+
 
 func on_card_selected_for_attack(opp_card):
 	do_card_battle(globals.card_attacking, opp_card)
 	$Board.disconnect_opponent_select_signals(self, "on_card_selected_for_attack")
+	
+	post_attack(globals.card_attacking)
 	globals.is_in_attack_choose_state = false
 	globals.card_attacking = null
 
@@ -65,14 +73,17 @@ func on_declare_attack(attacker_card_rep):
 	var opponent_creature_count = $Board.get_opponent_card_count() 
 	if opponent_creature_count < 1:
 		damage_opponent(attacker_card_rep.CARD_RESOURCE.POWER)
+		post_attack(attacker_card_rep)
 	elif opponent_creature_count == 1:
 		var opponent_creature_card_rep = $Board.get_opponent_only_card()
 		do_card_battle(attacker_card_rep, opponent_creature_card_rep)
+		post_attack(attacker_card_rep)
 	else:
 		globals.is_in_attack_choose_state = true
 		globals.card_attacking = attacker_card_rep
 		$Board.connect_opponent_select_signals(self, "on_card_selected_for_attack")
 		
+
 
 
 
