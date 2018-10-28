df79b7c285491cb4959549c39d474b511894b875
Dvlv <dvd292@googlemail.com>
2018-10-28 12:09:29 +0000

    remove print when press fuse

--- scripts/HandMenu.gd
+++ scripts/HandMenu.gd
@@ -2,17 +2,14 @@ extends Node2D
 
 signal card_played
 
+
 func _on_Cancel_pressed():
 	visible = false
+
 
 func _on_Play_pressed():
 	get_parent().play_card()
 
 
 func _on_Fuse_pressed():
-	print("fuse pressed")
 	get_parent().add_card_to_fusebox()
-
-
-func _on_Foos_pressed():
-	print("pres")
